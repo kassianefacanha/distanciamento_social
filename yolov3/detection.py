b6c3a5a0cee435cbf86d07ef1c2ff65dff8e83a6
Kassiane Façanha <kassianelf@alu.ufc.br>
2022-01-17 01:13:31 -0300

    TESTES FINAIS

--- yolov3/detection.py
+++ yolov3/detection.py
@@ -2,7 +2,7 @@ MODEL_PATH = "yolo-coco"
 MIN_CONF = 0.3
 NMS_THRESH = 0.3
 USE_GPU = False
-MIN_DISTANCE = 100
+MIN_DISTANCE = 50
 
 
 import numpy as np
--- yolov3/social_distance_detector.py
+++ yolov3/social_distance_detector.py
@@ -3,7 +3,7 @@ MODEL_PATH = "yolo-coco"
 MIN_CONF = 0.3
 NMS_THRESH = 0.4
 USE_GPU = False
-MIN_DISTANCE = 100
+MIN_DISTANCE = 50
 
 
 
@@ -12,9 +12,11 @@ from scipy.spatial import distance as dist
 import numpy as np
 import argparse
 import imutils
+import time
 import cv2
 import os
 
+start_time = time.time()
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input", type=str, default="",
 	help="path to (optional) input video file")
@@ -47,7 +49,6 @@ ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
 print("[INFO] accessing video stream...")
 vs = cv2.VideoCapture(args["input"] if args["input"] else 0)
 writer = None
-
 while True:
 	(grabbed, frame) = vs.read()
 
@@ -61,7 +62,6 @@ while True:
 	
 	violate = set()
 	abnormal = set()
-	print(len(results))
 
 	if len(results) >= 2:
 		centroids = np.array([r[2] for r in results])
@@ -69,10 +69,7 @@ while True:
 
 		for i in range(0, D.shape[0]):
 			for j in range(i + 1, D.shape[1]):
-				print(D[i, j])
 				if D[i, j] < MIN_DISTANCE:
-					print(D[i, j])
-					print("[INFO] tá entrando aqui")
 					violate.add(i)
 					violate.add(j)
 
@@ -108,6 +105,63 @@ while True:
 		writer = cv2.VideoWriter(args["output"], fourcc, 2,
 			(frame.shape[1], frame.shape[0]), True)
 
+		# if args["input"]:
+		# 		video = cv2.VideoCapture("teste2.avi");
+		# 		(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
 
+		# 		if int(major_ver)  < 3 :
+		# 			fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
+		# 			print ("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
+		# 		else :
+		# 			fps = video.get(cv2.CAP_PROP_FPS)
+		# 			print ("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+
+		# video.release()
+
+		# if not args["input"]:
+		# # Start default camera
+		# 	video = cv2.VideoCapture(0);
+
+		# 	# Find OpenCV version
+		# 	(major_ver, minor_ver, subminor_ver) = (cv2.__version__).split('.')
+
+		# 	# With webcam get(CV_CAP_PROP_FPS) does not work.
+		# 	# Let's see for ourselves.
+
+		# 	if int(major_ver)  < 3 :
+		# 		fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
+		# 		print("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
+		# 	else :
+		# 		fps = video.get(cv2.CAP_PROP_FPS)
+		# 		print("Frames per second using video.get(cv2.CAP_PROP_FPS) : {0}".format(fps))
+
+		# 	# Number of frames to capture
+		# 	num_frames = 120;
+
+		# 	print("Capturing {0} frames".format(num_frames))
+
+		# 	# Start time
+		# 	start = time.time()
+
+		# 	# Grab a few frames
+		# 	for i in range(0, num_frames) :
+		# 		ret, frame = video.read()
+
+		# 	# End time
+		# 	end = time.time()
+
+		# 	# Time elapsed
+		# 	seconds = end - start
+		# 	print ("Time taken : {0} seconds".format(seconds))
+
+		# 	# Calculate frames per second
+		# 	fps  = num_frames / seconds
+		# 	print("Estimated frames per second : {0}".format(fps))
+
+		# 	# Release video
+		# 	video.release()
+
+
+	
 	if writer is not None:
 		writer.write(frame)
